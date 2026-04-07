8c081fb3b884722aea9cbec5650d3ae429ce5227
jinny-do <sonmojiri7@gamil.com>
2026-04-07 18:14:41 +0900

    260407 for

--- tng/06_for.js
+++ tng/06_for.js
@@ -43,3 +43,28 @@ for (let k = 1; k < 6; k++) {
 //   star += '*';
 //   console.log(`${space.slice(1, space.length - i)}${star}`);
 // }
+
+for (let a = 0; a < 5; a++) {
+  let space = "";
+  let star3 = "";
+
+  for (let b = 0; b < 4 - a; b++) {
+    space += " ";
+  }
+  for (let c = 0; c < a * 2 + 1; c++) {
+    star3 += "*";
+  }
+  console.log(space + star3);
+}
+for (let d = 0; d < 5; d++) {
+  let space1 = "";
+  let star31 = "";
+
+  for (let e = 0; e < d; e++) {
+    space1 += " ";
+  }
+  for (let f = 0; f < (5 - d) * 2 - 1; f++) {
+    star31 += "*";
+  }
+  console.log(space1 + star31);
+}
